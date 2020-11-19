8b455773bd43fd8a8589308c0044c40db4fea025
bguilas <bryanjguilas@gmail.com>
2020-11-18 21:59:24 -0800

    added more squares and made the drop random

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
 
+
+const width = 800;
+const height = 600;
 const engine = Engine.create();
 const {world} = engine;
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600,
+        width,
+        height,
     }
 });
 
@@ -15,7 +18,7 @@ Render.run(render);
 Runner.run(Runner.create(), engine);
 World.add(world, MouseConstraint.create(engine, {
     mouse: Mouse.create(render.canvas)
-    }));
+}));
 
 // Walls
 
@@ -26,4 +29,10 @@ const walls = [
     Bodies.rectangle(800, 300, 40, 600, {isStatic: true}),
 ];
 World.add(world, walls);
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+
+//Random Shapes
+for (let i = 0; i < 20; i++) {
+    World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
+}
+
+
