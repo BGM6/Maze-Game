97489abb9e93ba7c9320f236505daf5881a16e8a
bguilas <bryanjguilas@gmail.com>
2020-11-19 21:24:25 -0800

    grid logic

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const {Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
+const {Engine, Render, Runner, World, Bodies,} = Matter;
 
-const width = 800;
+const cells = 3;
+const width = 600;
 const height = 600;
+
 const engine = Engine.create();
 const {world} = engine;
-const render = Render.create({
+let render;
+render = Render.create({
     element: document.body,
     engine: engine,
     options: {
@@ -16,56 +19,51 @@ const render = Render.create({
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}));
 
 // Walls
-
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
-    Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
-    Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
-    Bodies.rectangle(800, 300, 40, 600, {isStatic: true}),
+
+    Bodies.rectangle(width / 2, 0, width, 40, {isStatic: true}),
+    Bodies.rectangle(width / 2, height, width, 40, {isStatic: true}),
+    Bodies.rectangle(0, height / 2, 40, height, {isStatic: true}),
+    Bodies.rectangle(width, height / 2, 40, height, {isStatic: true}),
 ];
 World.add(world, walls);
 
-//Random Shapes
-for (let i = 0; i < 20; i++) {
-    if (Math.random() > 0.3) {
-        World.add(
-            world,
-            Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, {
-                render: {
-                    // fillStyle: 'teal',
-                }
-            }));
-    }
-    if (Math.random() < 0.4 > 0.6) {
-        World.add(
-            world,
-            Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-                render: {
-                    // fillStyle: 'purple',
-                }
-            })
-        );
-    }
-    if (Math.random() > 0.7) {
-        World.add(
-            world,
-            Bodies.trapezoid(Math.random() * width, Math.random() * height, 50, 50, 1, {
-                render: {
-                    // fillStyle: 'yellow',
-                }
-            }));
-    } else {
-        World.add(
-            world,
-            Bodies.polygon(Math.random() * width, Math.random() * height, 8, 35)
-        )
-    }
+// Maze generation
+
+const grid = Array(cells)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+
+const stepThroughCell = (row, column) => {
+//   If I have visited the cell at [row, column], then return
+
+//    Mark this cell as being visited by true/false
+
+//    Assemble randomly-ordered list of neighbors
+
 }
 
+stepThroughCell(startRow, startColumn)
 
+// Same as above code block
+// for (let i = 0; i < 3; i++) {
+//     grid.push([]);
+//     for (let j = 0; j < 3; j++) {
+//         grid[i].push(false);
+//     }
+// }
 
